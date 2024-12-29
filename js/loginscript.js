655a3e088861d817d798cbdb0d4e498d9fc4b556
aarjuuu <aarjutaujale1@gmail.com>
2024-12-29 06:32:15 +0545

    gmailValidity

--- js/loginscript.js
+++ js/loginscript.js
@@ -1,5 +1,7 @@
 const icon = document.getElementById("icon");
 const pass = document.getElementById("password");
+const gmailInput = document.getElementById("gmail");
+const loginBtn = document.getElementById("login-btn");
 
 icon.onclick = function() {
     if(pass.type === "password") {
@@ -11,3 +13,17 @@ icon.onclick = function() {
         icon.src = "../assests/icons/lock-solid-24.png";
     }
 }
+
+// Validate Gmail input
+gmailInput.addEventListener("input", function () {
+    const gmailPattern = /^[a-zA-Z0-9._%+-]+@(gmail\.com|student\.ku\.edu\.np)$/;
+    const isValid = gmailPattern.test(gmailInput.value);
+
+    if (isValid) {
+        gmailInput.parentElement.classList.remove("invalid");
+        loginBtn.disabled = false;
+    } else {
+        gmailInput.parentElement.classList.add("invalid");
+        loginBtn.disabled = true;
+    }
+});
