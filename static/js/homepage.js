5f8b1c586577a50d1c1c2219decd3d806ed58bb6
fedupGenJi <thakur.aakash569@gmail.com>
2025-01-06 05:31:13 +0545

    homepage with transition

---
+++ static/js/homepage.js
@@ -0,0 +1,39 @@
+const FADE_DURATION = 800;
+const RESET_DELAY = 5000;
+
+let fadeTimeout;
+
+function fadeScreen() {
+    const introContainer = document.getElementById('intro-container');
+    const otherElements = document.querySelectorAll('.navbarpostt, .homepage-container');
+    const body = document.body;
+
+    introContainer.classList.add('faded');
+    body.classList.add('bg-faded');
+
+    setTimeout(() => {
+        otherElements.forEach(element => element.classList.remove('hidden'));
+    }, FADE_DURATION);
+}
+
+function resetScreen() {
+    clearTimeout(fadeTimeout);
+    const introContainer = document.getElementById('intro-container');
+    const otherElements = document.querySelectorAll('.navbarpostt, .homepage-container');
+    const body = document.body;
+
+    introContainer.classList.remove('faded');
+    body.classList.remove('bg-faded');
+    otherElements.forEach(element => element.classList.add('hidden'));
+}
+
+function handleScreenFade() {
+    fadeScreen();
+    fadeTimeout = setTimeout(() => {
+        resetScreen();
+    }, RESET_DELAY + FADE_DURATION);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', handleScreenFade);
+});
