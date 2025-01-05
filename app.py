365119579f436c0190a09cba723c215a13cbad0c
aarjuuu <aarjutaujale1@gmail.com>
2025-01-05 18:24:11 +0545

    using database

--- app.py
+++ app.py
@@ -1,26 +1,65 @@
 from flask import Flask, render_template
+import mysql.connector
+import config
+import base64
 
 app = Flask(__name__)
 
-def get_admin_details():
+def get_admin_details(gmail):
+    connection = mysql.connector.connect(
+            host = "localhost",
+            user = config.user,
+            passwd = config.passwd,
+            database="iCardSISDB"
+        )
+    
+    try:
+        cursor = connection.cursor(dictionary=True)  
+        query = "SELECT adminId, Gmail, userName, status, photo FROM adminLogin;" 
+
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+
+        for row in results:
+            admin_id = row['adminId']
+            gmail = row['Gmail']
+            user_name = row['userName']
+            status = row['status']
+            photo = row['photo']
+
+        if status == "KU-Admin":
+            post = "Administrator"
+        else:
+            post = "Librarian"
+
+        if photo:
+                photo = base64.b64encode(photo).decode('utf-8')
+    finally:
+        cursor.close()
+        connection.close()
+
+    
     admin_details = {
-        "admin_name": "John Doe",
-        "admin_position": "Administrator",
-        "admin_id": "KU-Admin 7788",
-        "admin_photo_url": "/static/images/admin.png",
-        "admin_email": "john.doe@example.com",
-        "admin_status": "Admin-Operator"
+        "admin_name": user_name,
+        "admin_position": post,
+        "admin_id": admin_id,
+        "admin_photo_url": f"data:image/jpeg;base64,{photo}" if photo else "/static/images/admin.png",
+        "admin_email": gmail,
+        "admin_status": status
     }
     return admin_details
 
 @app.route('/')
 def home():
-    admin_details = get_admin_details()
+    id = "thakur.aakash569@gmail.com"
+    admin_details = get_admin_details(id)
     return render_template('home.html',**admin_details)
 
 @app.route('/about')
 def about():
-    admin_details = get_admin_details()
+    id = "thakur.aakash569@gmail.com"
+    admin_details = get_admin_details(id)
     return render_template('about.html',**admin_details)
 
 if __name__ == '__main__':
