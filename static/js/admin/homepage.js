296215266ded5d80cf16008dab10d7786d4e1741
fedupGenJi <thakur.aakash569@gmail.com>
2025-01-12 00:43:24 +0545

    session logout

--- static/js/admin/homepage.js
+++ static/js/admin/homepage.js
@@ -57,3 +57,7 @@ function toggleModal() {
     const modal = document.getElementById("adminModal");
     modal.classList.toggle("active");
 }
+
+function logout() {
+    window.location.href = '/logout';
+}
